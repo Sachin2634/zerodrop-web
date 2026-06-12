aa57b0fd5bd79fa5510844b7deb965b24b2c6f41
Sachin2634 <105629187+Sachin2634@users.noreply.github.com>
2026-06-12 16:34:57 +0530

    feat: contact cards optimization + color base file added

--- src/app/contactus/components/ContactCards.tsx
+++ src/app/contactus/components/ContactCards.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
-const ContactCards = ({props}: any) => {
+interface ContactCardProps {
+  icon: React.ReactNode;
+  heading: string;
+  description: string;
+}
+
+
+const ContactCards = ({ icon, heading, description }: ContactCardProps) => {
   return (
     <div className='flex flex-col gap-3 border border-gray-300 px-10 py-6 items-center w-60 rounded-xl shadow-lg'>
-      <div className='text-white rounded-full p-3 bg-[#419E5B]'>{props.icon}</div>
-      <div className='text-sm text-[#237039] font-semibold'>{props.heading}</div>
-      <div className='text-xs text-center leading-5 font-light'>{props.description}</div>
+      <div className='text-white rounded-full p-3 bg-[#419E5B]'>{icon}</div>
+      <div className='text-sm text-[#237039] font-semibold'>{heading}</div>
+      <div className='text-xs text-center leading-5 font-light'>{description}</div>
     </div>
   )
 }
---
+++ src/constants/colors.ts
@@ -0,0 +1,6 @@
+// Create src/constants/colors.ts
+export const COLORS = {
+  primary: '#419E5B',
+  secondary: '#4cabc0',
+  dark: '#237039',
+} as const;
